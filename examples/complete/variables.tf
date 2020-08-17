73d4c2ff81a253de9b256197cbc579dc4a50e088
Eric Berg <eberg@bergbrains.com>
2020-08-17 12:59:31 -0400

    Added ignore_members variable and use it in opsgenie_team resource (#3)
    * Added ignore_members variable and use it in opsgenie_team resource
    
    * switched to passing `ignore_members` in with team block

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -48,3 +48,9 @@ variable "opsgenie_team" {
   description = "This variable is used to configure opsgenie team."
   default     = {}
 }
+
+variable "ignore_members" {
+  type        = bool
+  description = "Set to true to ignore any configured member blocks and any team member added/updated/removed via OpsGenie web UI"
+  default     = false
+}
