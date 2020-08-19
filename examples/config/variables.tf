2b15ecadf3f5f68cf0af3f70cd59354a860ff6bc
Dan Meyers <danjbh@users.noreply.github.com>
2020-08-18 18:06:50 -0700

    Defaulting Opsgenie API key to null so that it will use the environment variable

--- examples/config/variables.tf
+++ examples/config/variables.tf
@@ -1,5 +1,6 @@
 variable "opsgenie_provider_api_key" {
   type        = string
   description = "The API Key for the Opsgenie Integration. If omitted, the OPSGENIE_API_KEY environment variable is used."
+  default     = null
 }
 
