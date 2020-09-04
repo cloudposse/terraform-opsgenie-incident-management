c80c76ce58499e4b1ca127837fad47381f7ba003
Eric Berg <eric.berg@brace.ai>
2020-09-04 13:50:51 -0400

    Added ignore_members and delete_default_resource attributes to teams

--- modules/team/main.tf
+++ modules/team/main.tf
@@ -1,5 +1,6 @@
 resource "opsgenie_team" "this" {
-  name           = var.team.name
-  description    = try(var.team.description, var.team.name)
-  ignore_members = try(var.team.ignore_members, false)
+  name                     = var.team.name
+  description              = try(var.team.description, var.team.name)
+  ignore_members           = try(var.team.ignore_members, false)
+  delete_default_resources = try(var.team.delete_default_resources, false)
 }
