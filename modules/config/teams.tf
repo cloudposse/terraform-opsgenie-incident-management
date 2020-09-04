ad731340184249ced28bec814106ab3ec83a9b98
Eric Berg <eric.berg@brace.ai>
2020-09-04 14:51:48 -0400

    added default of team.name for team.description in teams module

--- modules/config/teams.tf
+++ modules/config/teams.tf
@@ -4,7 +4,7 @@ resource "opsgenie_team" "this" {
   }
 
   name                     = each.value.name
-  description              = each.value.description
+  description              = try(each.value.description, var.team.name)
   ignore_members           = try(each.value.ignore_members, false)
   delete_default_resources = try(each.value.delete_default_resources, false)
 }
