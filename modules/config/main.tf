5b3ec41f851499fbca71a0a04663e5958fb5b27b
aknysh <andriy.knysh@gmail.com>
2020-10-15 11:53:59 -0400

    Update config

--- modules/config/main.tf
+++ modules/config/main.tf
@@ -1,7 +1,7 @@
 locals {
   alert_policies         = try(var.opsgenie_resources.alert_policies, [])
   api_integrations       = try(var.opsgenie_resources.api_integrations, [])
-  escalations            = try(var.opsgenie_resources.escalations, [])
+  escalations            = var.opsgenie_resources.escalations != null ? var.opsgenie_resources.escalations : []
   notification_policies  = try(var.opsgenie_resources.notification_policies, [])
   team_routing_rules     = try(var.opsgenie_resources.team_routing_rules, [])
   teams                  = try(var.opsgenie_resources.teams, [])
