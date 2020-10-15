7cccd5850f48575354c575b436086edb79215943
aknysh <andriy.knysh@gmail.com>
2020-10-15 11:58:48 -0400

    Update config

--- modules/config/main.tf
+++ modules/config/main.tf
@@ -1,11 +1,11 @@
 locals {
-  alert_policies         = try(var.opsgenie_resources.alert_policies, [])
-  api_integrations       = try(var.opsgenie_resources.api_integrations, [])
-  escalations            = var.opsgenie_resources.escalations != null ? var.opsgenie_resources.escalations : []
-  notification_policies  = try(var.opsgenie_resources.notification_policies, [])
-  team_routing_rules     = try(var.opsgenie_resources.team_routing_rules, [])
-  teams                  = try(var.opsgenie_resources.teams, [])
-  users                  = try(var.opsgenie_resources.users, [])
-  services               = try(var.opsgenie_resources.services, [])
-  service_incident_rules = try(var.opsgenie_resources.service_incident_rules, [])
+  alert_policies         = try(var.opsgenie_resources.alert_policies, null) != null ? var.opsgenie_resources.alert_policies : []
+  api_integrations       = try(var.opsgenie_resources.api_integrations, null) != null ? var.opsgenie_resources.api_integrations : []
+  escalations            = try(var.opsgenie_resources.escalations, null) != null ? var.opsgenie_resources.escalations : []
+  notification_policies  = try(var.opsgenie_resources.notification_policies, null) != null ? var.opsgenie_resources.notification_policies : []
+  team_routing_rules     = try(var.opsgenie_resources.team_routing_rules, null) != null ? var.opsgenie_resources.team_routing_rules : []
+  teams                  = try(var.opsgenie_resources.teams, null) != null ? var.opsgenie_resources.teams : []
+  users                  = try(var.opsgenie_resources.users, null) != null ? var.opsgenie_resources.users : []
+  services               = try(var.opsgenie_resources.services, null) != null ? var.opsgenie_resources.services : []
+  service_incident_rules = try(var.opsgenie_resources.service_incident_rules, null) != null ? var.opsgenie_resources.service_incident_rules : []
 }
