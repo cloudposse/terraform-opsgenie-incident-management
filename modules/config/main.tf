3fa601d0aa0c48e4bc370db123d8adf7728e1883
aknysh <andriy.knysh@gmail.com>
2020-10-16 18:05:28 -0400

    Update config

--- modules/config/main.tf
+++ modules/config/main.tf
@@ -19,14 +19,14 @@
 # https://github.com/hashicorp/terraform/issues/22405
 
 locals {
-  alert_policies         = lookup(var.opsgenie_resources, "alert_policies", []) != null ? tolist(lookup(var.opsgenie_resources, "alert_policies", [])) : list()
-  api_integrations       = lookup(var.opsgenie_resources, "api_integrations", []) != null ? tolist(lookup(var.opsgenie_resources, "api_integrations", [])) : list()
-  escalations            = lookup(var.opsgenie_resources, "escalations", []) != null ? tolist(lookup(var.opsgenie_resources, "escalations", [])) : list()
-  notification_policies  = lookup(var.opsgenie_resources, "notification_policies", []) != null ? tolist(lookup(var.opsgenie_resources, "notification_policies", [])) : list()
-  team_routing_rules     = lookup(var.opsgenie_resources, "team_routing_rules", []) != null ? tolist(lookup(var.opsgenie_resources, "team_routing_rules", [])) : list()
-  teams                  = lookup(var.opsgenie_resources, "teams", []) != null ? tolist(lookup(var.opsgenie_resources, "teams", [])) : list()
-  users                  = lookup(var.opsgenie_resources, "users", []) != null ? tolist(lookup(var.opsgenie_resources, "users", [])) : list()
-  existing_users         = lookup(var.opsgenie_resources, "existing_users", []) != null ? tolist(lookup(var.opsgenie_resources, "existing_users", [])) : list()
-  services               = lookup(var.opsgenie_resources, "services", []) != null ? tolist(lookup(var.opsgenie_resources, "services", [])) : list()
-  service_incident_rules = lookup(var.opsgenie_resources, "service_incident_rules", []) != null ? tolist(lookup(var.opsgenie_resources, "service_incident_rules", [])) : list()
+  alert_policies         = lookup(var.opsgenie_resources, "alert_policies", []) != null ? tolist(lookup(var.opsgenie_resources, "alert_policies", [])) : []
+  api_integrations       = lookup(var.opsgenie_resources, "api_integrations", []) != null ? tolist(lookup(var.opsgenie_resources, "api_integrations", [])) : []
+  escalations            = lookup(var.opsgenie_resources, "escalations", []) != null ? tolist(lookup(var.opsgenie_resources, "escalations", [])) : []
+  notification_policies  = lookup(var.opsgenie_resources, "notification_policies", []) != null ? tolist(lookup(var.opsgenie_resources, "notification_policies", [])) : []
+  team_routing_rules     = lookup(var.opsgenie_resources, "team_routing_rules", []) != null ? tolist(lookup(var.opsgenie_resources, "team_routing_rules", [])) : []
+  teams                  = lookup(var.opsgenie_resources, "teams", []) != null ? tolist(lookup(var.opsgenie_resources, "teams", [])) : []
+  users                  = lookup(var.opsgenie_resources, "users", []) != null ? tolist(lookup(var.opsgenie_resources, "users", [])) : []
+  existing_users         = lookup(var.opsgenie_resources, "existing_users", []) != null ? tolist(lookup(var.opsgenie_resources, "existing_users", [])) : []
+  services               = lookup(var.opsgenie_resources, "services", []) != null ? tolist(lookup(var.opsgenie_resources, "services", [])) : []
+  service_incident_rules = lookup(var.opsgenie_resources, "service_incident_rules", []) != null ? tolist(lookup(var.opsgenie_resources, "service_incident_rules", [])) : []
 }
