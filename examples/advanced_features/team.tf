960b892e84304a3a3f854c639a3a05cf1fbbbe30
Yonatan Koren <me@yonatankoren.com>
2021-06-07 10:39:21 -0400

    Remove unused sub-team from advanced_features example; give expanded team description in advanced_features example.

--- examples/advanced_features/team.tf
+++ examples/advanced_features/team.tf
@@ -2,17 +2,8 @@ module "team" {
   source = "../../modules/team"
 
   team = {
-    name           = module.this.id
-    description    = "team-description"
+    name           = "${module.this.id}.security"
+    description    = "${module.this.id} Security Team"
     ignore_members = false
-  }
-}
-
-module "sub_team" {
-  source = "../../modules/team"
-
-  team = {
-    name        = "${module.this.id}.team"
-    description = "sub-team-description"
   }
 }
