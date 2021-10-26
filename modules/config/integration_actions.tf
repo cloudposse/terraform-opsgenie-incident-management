0606c078699b78cafe33e79f9d6d521cab83deb1
nate-selzer <53010109+nate-selzer@users.noreply.github.com>
2021-10-26 19:13:25 -0400

    Change for_each to avoid nested maps typing_error
    Related to https://stackoverflow.com/questions/66970542/inconsistent-conditional-result-types-with-locals-templatization

--- modules/config/integration_actions.tf
+++ modules/config/integration_actions.tf
@@ -1,5 +1,5 @@
 resource "opsgenie_integration_action" "this" {
-  for_each = module.this.enabled ? { for integration_action in local.integration_actions : integration_action.name => integration_action } : {}
+  for_each = { for integration_action in local.integration_actions : integration_action.name => integration_action if module.this.enabled }
 
   # Look up our integration id by name
   integration_id = try(opsgenie_api_integration.this[each.value.integration_name].id, null)
