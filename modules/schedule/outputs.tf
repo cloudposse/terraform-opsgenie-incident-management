8de977fb33cd93e692e95ec78e44c3d6b969ebaa
nitrocode <nitrocode@users.noreply.github.com>
2021-12-31 01:45:01 -0500

    Update modules/schedule/outputs.tf
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>

--- modules/schedule/outputs.tf
+++ modules/schedule/outputs.tf
@@ -1,4 +1,4 @@
 output "schedule_id" {
   description = "The ID of the Opsgenie Schedule"
-  value       = try(opsgenie_schedule.default[*].id, null)
+  value       = try(opsgenie_schedule.default[0].id, null)
 }
