a38142fcc53c20c93e37b9605be0605af0ab8cde
nitrocode <nitrocode@users.noreply.github.com>
2021-12-31 01:48:17 -0500

    Update modules/team_routing_rule/main.tf
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>

--- modules/team_routing_rule/main.tf
+++ modules/team_routing_rule/main.tf
@@ -30,7 +30,7 @@ resource "opsgenie_team_routing_rule" "this" {
         for_each = var.team_routing_rule.time_restriction.type == "time-of-day" ? try(var.team_routing_rule.time_restriction.restrictions, []) : []
         content {
           start_hour = restriction.value.start_hour
-          start_min  = try(restriction.value.start_min, 0)
+          start_min  = restriction.value.start_min
           end_hour   = try(restriction.value.end_hour, 17)
           end_min    = try(restriction.value.end_min, 00)
         }
