bba05bd0ceb9b0e5aba438db04eb2274b865f9ed
Diego Rabatone Oliveira <diego.rabatone@nubank.com.br>
2024-06-12 12:52:08 -0300

    [config] Add squad intermediate abstraction

--- examples/config/main.tf
+++ examples/config/main.tf
@@ -3,12 +3,21 @@ provider "opsgenie" {
 }
 
 locals {
-  # Build a map of our various Opsgenie resources that will be used to iterate over each module
-  opsgenie_resources = merge([
-    for resource_file in fileset(path.cwd, "resources/*.yaml") : {
+  # load the config for each squad individually
+  # Temporary variable to hold the config for each squad
+  squads_configs = [
+    for resource_file in fileset(path.cwd, "squads/*/*.yaml") : {
       for k, v in yamldecode(file(resource_file)) : k => v
     }
-  ]...)
+  ]
+
+  # Build a map of our various Opsgenie resources that will be used to iterate over each module
+  # by "joining" the configs from many squads into a single map
+  opsgenie_resources = {
+    for key in distinct(flatten([for x in local.squads_configs : keys(x)])) :
+    key => flatten([
+    for m, n in local.squads_configs : n[key] if keys(n)[0] == key])
+  }
 }
 
 module "opsgenie_config" {
