fb970f913d43b9d355659bd650762d16d58e2daf
Benjamin Smith <ben.smith.developer@gmail.com>
2024-07-22 11:19:17 -0700

    update hcl

--- main.tf
+++ main.tf
@@ -4,43 +4,43 @@ provider "opsgenie" {
 
 module "alert_policy" {
   source = "./modules/alert_policy"
-  
+
   alert_policy = var.alert_policy
-  context = module.this.context
+  context      = module.this.context
 }
 
 module "api_integration" {
   source = "./modules/api_integration"
 
   api_integration = var.api_integration
-  context = module.this.context
+  context         = module.this.context
 }
 
 module "escalation" {
   source = "./modules/escalation"
 
   escalation = var.escalation
-  context = module.this.context
+  context    = module.this.context
 }
 
 module "integration_action" {
   source = "./modules/integration_action"
 
   integration_action = var.integration_action
-  context = module.this.context
+  context            = module.this.context
 }
 
 module "notification_policy" {
   source = "./modules/notification_policy"
 
   notification_policy = var.notification_policy
-  context = module.this.context
+  context             = module.this.context
 }
 
 module "team" {
   source = "./modules/team"
 
-  team = var.team
+  team    = var.team
   context = module.this.context
 }
 
@@ -48,13 +48,13 @@ module "team_routing_rule" {
   source = "./modules/team_routing_rule"
 
   team_routing_rule = var.team_routing_rule
-  context = module.this.context
+  context           = module.this.context
 }
 
 module "user" {
   source = "./modules/user"
 
-  user = var.user
+  user    = var.user
   context = module.this.context
 }
 
@@ -69,5 +69,5 @@ module "service_incident_rule" {
   source = "./modules/service_incident_rule"
 
   service_incident_rule = var.service_incident_rule
-  context = module.this.context
+  context               = module.this.context
 }
